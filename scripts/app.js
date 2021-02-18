e1071de09dfe46b06acede00fe4a01e9d3a440cc
michael_edwards1994@hotmail.com <michael_edwards1994@hotmail.com>
2021-02-18 20:43:33 +0000

    basic grid

--- scripts/app.js
+++ scripts/app.js
@@ -1,12 +1,24 @@
 function init() {
 
-  console.log('CONTENT LOADED')
+  const grid = document.querySelector('.grid')
+
+  const width = 10
+  const height = 10
+  const cellCount = width * height
+
+  const cells = []
+
+  function createGrid() {
+    for (let i = 0; i < cellCount; i++) {
+      const cell = document.createElement('div')
+      cell.textContent = i
+      grid.appendChild(cell)
+      cells.push(cell)
+    }
+  }
 
 
-
-
-
-
+  createGrid()
 
 }
 
